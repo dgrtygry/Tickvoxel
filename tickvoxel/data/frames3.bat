c9c53b14eb29488d79d53d8b122af4bfb1787cb9
Tick <76979689+dgrtygry@users.noreply.github.com>
2023-06-08 14:48:05 -0500

    Create frames3.bat

---
+++ tickvoxel/data/frames3.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:loop
+cls
+echo              & echo      ^^^     & echo       ^^^
+echo              & echo    ^^^^^^^   & echo     ^^^^^^^
+echo              & echo   ^^^^^^^^   & echo   ^^^^^^^^
+echo        __O__ & echo  ^^^^^^^^   & echo  ^^^^^^^^
+echo       /|     & echo              & echo        
+echo       / \    & echo              & echo
+
+timeout /t 0.1 >nul
+goto loop
